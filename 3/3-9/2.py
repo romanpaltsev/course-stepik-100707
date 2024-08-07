14796bfc91f104936392e210abc5ec8068eefaf5
Roman Paltsev <roman.palcev@gmail.com>
2024-08-07 10:51:37 +0300

    add block 3-9

---
+++ 3/3-9/2.py
@@ -0,0 +1,7 @@
+line1 = input().split()
+line2 = input().split()
+line3 = input().split()
+
+lst = [line1, line2, line3]
+
+print(lst)
---
+++ 3/3-9/6.py
@@ -0,0 +1,6 @@
+line1 = list(map(int, input().split()))
+line2 = list(map(int, input().split()))
+line3 = list(map(int, input().split()))
+
+lst = [line1, line2, line3]
+print(lst[0][3], lst[1][3], lst[2][3])
